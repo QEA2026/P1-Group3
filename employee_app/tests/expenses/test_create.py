a0cb8a90ec570d6e62c1408ad164367ed670b797
Benson Zheng <bensonzheng2003@gmail.com>
2026-07-17 20:06:31 -0400

    implement test_create_invalidShouldRaiseDatabaseError and test_create_failedConnectionShouldRaiseDatabaseError

--- employee_app/tests/expenses/test_create.py
+++ employee_app/tests/expenses/test_create.py
@@ -1,14 +1,17 @@
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, patch
 from controllers.expenses import create
 from models.expenses import Expense
+from utils.exceptions.database_exception import DatabaseException
+import sqlite3
+import pytest
 
 class TestCreate:
     @patch("controllers.expenses.get_connection")
     def test_create_validShouldReturnApprovals(self, mock_get_connection):
-        conn = Mock()
-        cursor = Mock()
+        conn = MagicMock()
+        cursor = MagicMock()
 
-        mock_get_connection.return_value = conn
+        mock_get_connection.return_value.__enter__.return_value = conn
         conn.execute.return_value = cursor
         cursor.lastrowid = 5
 
@@ -17,6 +20,34 @@ class TestCreate:
 
         assert resulting_expense.__str__() == (f"Expense #5: $10 for description "
         f"on may15")
+
+    @patch("controllers.expenses.get_connection")
+    def test_create_invalidShouldRaiseDatabaseError(self, mock_get_connection):
+        conn = MagicMock()
+
+        mock_get_connection.return_value.__enter__.return_value = conn
+        conn.execute.side_effect = sqlite3.IntegrityError()
+
+        expense = Expense(1, 1, "description", "date")
+
+        with pytest.raises(DatabaseException) as e:
+            create(expense)
+        
+        assert str(e.value) == "Contraints violated"
+
+
+    @patch("controllers.expenses.get_connection")
+    def test_create_failedConnectionShouldRaiseDatabaseError(self, mock_get_connection):
+        mock_get_connection.side_effect = sqlite3.DatabaseError
+
+        expense = Expense(1, 1, "description", "date")
+
+        with pytest.raises(DatabaseException) as e:
+            create(expense)
+
+        assert str(e.value) == "Database execution failed"
+
+        
 
 
 
