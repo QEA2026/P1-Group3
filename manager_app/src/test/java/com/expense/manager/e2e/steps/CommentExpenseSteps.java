bbfb0e4e0c422d9353700d583015eab62b7ed2d1
oscar <okardon@elon.edu>
2026-07-28 10:45:00 -0400

    fixes for e2e

--- manager_app/src/test/java/com/expense/manager/e2e/steps/CommentExpenseSteps.java
+++ manager_app/src/test/java/com/expense/manager/e2e/steps/CommentExpenseSteps.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 import com.expense.manager.e2e.context.TestContext;
 import com.expense.manager.e2e.hooks.Hooks;
+import com.expense.manager.e2e.pages.EmployeeDashboardPage;
 import com.expense.manager.e2e.pages.ManagerDashboardPage;
 
 import io.cucumber.java.Before;
@@ -13,6 +14,7 @@ import io.cucumber.java.en.When;
 public class CommentExpenseSteps {
     private WebDriver driver;
     private ManagerDashboardPage managerDashboardPage;
+    private EmployeeDashboardPage employeeDashboardPage;
     private TestContext context;
 
     public CommentExpenseSteps(TestContext context) {
@@ -23,6 +25,7 @@ public class CommentExpenseSteps {
     public void setUpPages() {
         driver = Hooks.driver;
         managerDashboardPage = new ManagerDashboardPage(driver);
+        employeeDashboardPage = new EmployeeDashboardPage(driver);
     }
 
     
@@ -31,7 +34,8 @@ public class CommentExpenseSteps {
         managerDashboardPage.addComment(comment);
     }
     @Then("the expense review comment should be {string} on the employee history page")
-    public void the_expense_review_comment_should_be_on_the_employee_history_page(String string) {
-        
+    public void the_expense_review_comment_should_be_on_the_employee_history_page(String comment) {
+        employeeDashboardPage.reviewExpense(context.getExpenseDescription());
+        employeeDashboardPage.verifyComment(context.getExpenseDescription(), comment);
     }
 }
